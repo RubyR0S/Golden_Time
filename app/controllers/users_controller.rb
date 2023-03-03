1ca12b0fb6bcc522fc73f1243e03687ecd2f6c59
Ros <Fedorovch>
2023-03-03 11:14:13 +0200

    Added small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       sign_in @user
-      flash[:succsess] = "Your profile succsessfully registred!"
+      flash[:success] = "Your profile succsessfully registred!"
       redirect_to root_path
     else
       render :new
