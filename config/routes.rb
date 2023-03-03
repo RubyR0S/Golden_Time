db7d397bbd6c3220938ec5df3e9d2fd1172bd708
Ros <Fedorovch>
2023-03-03 10:15:13 +0200

    Created main page and users registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'mains#index'
+
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create edit update]
 end
